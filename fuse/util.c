fe7a4fac87c7468ceee67a3bb3537b06808b162b
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-05-20 14:31:22 +0200

    Timestamp parsing code

--- fuse/util.c
+++ fuse/util.c
@@ -1,11 +1,14 @@
 #include <sys/types.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <assert.h>
 #include <sys/mman.h>
 #include <err.h>
+#include <time.h>
 
 #include "util.h"
+#include "vfat.h"
 
 
 uintptr_t page_floor(uintptr_t offset) {
@@ -51,4 +54,55 @@ void unmap(void* buf, size_t size)
     size_t len = end - start;
     if (munmap((void*)start, len) < 0)
         err(1, "munmap failed");
+}
+
+void clear_time(struct tm *time)
+{
+    assert(time);
+    memset(time, 0, sizeof(*time));
+}
+
+void vfat_parse_date(const uint16_t date, struct tm *out)
+{
+    out->tm_mday = (date & 0xF);
+    out->tm_mon = (date >> 4) & 0b111;
+    out->tm_year = (date >> 8) & 0x3F;
+
+    assert(out->tm_mday > 0 && out->tm_mday < 32);
+    assert(out->tm_mon > 0 && out->tm_mon < 13);
+}
+
+void vfat_parse_time(const uint16_t time, struct tm *out)
+{
+    out->tm_hour = (time >> 10);
+    out->tm_min = (time >> 4) & 0b111111;
+    out->tm_sec = (time & 0xF) * 2;
+
+    assert(out->tm_hour > 0 && out->tm_hour < 24);
+    assert(out->tm_sec > 0 && out->tm_sec < 60);
+    assert(out->tm_min > 0 && out->tm_min < 60);
+}
+
+void vfat_parse_timestamp(const struct fat32_direntry *dir, struct stat *out)
+{
+    assert(dir);
+    assert(out);
+
+    struct tm atime;
+    struct tm mtime;
+    struct tm ctime;
+
+    clear_time(&atime);
+    clear_time(&mtime);
+    clear_time(&ctime);
+
+    vfat_parse_date(dir->atime_date, &atime);
+    vfat_parse_date(dir->ctime_date, &ctime);
+    vfat_parse_time(dir->ctime_time, &ctime);
+    vfat_parse_date(dir->mtime_date, &mtime);
+    vfat_parse_time(dir->mtime_time, &mtime);
+
+    out->st_ctime = mktime(&ctime);
+    out->st_mtime = mktime(&mtime);
+    out->st_atime = mktime(&atime);
 }
--- fuse/util.h
+++ fuse/util.h
@@ -1,9 +1,18 @@
 #ifndef H_UTIL
 #define H_UTIL
 
+#include <stdint.h>
+#include <sys/types.h>
+#include <wchar.h>
+
+#include "vfat.h"
+
 void* mmap_file(int fd, off_t offset, size_t size);
 void unmap(void* buf, size_t size);
 
+void vfat_parse_timestamp(const struct fat32_direntry *dir, struct stat *out);
+void vfat_parse_time(const uint16_t time, struct tm *out);
+void vfat_parse_date(const uint16_t date, struct tm *out);
 
 #define MIN(a, b) ((a) < (b) ? b : a)
 
